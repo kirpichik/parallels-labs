2272651383b51f43400115b329e679a3a315e640
Kirill Polushin <kirill@polushin.org>
2018-10-30 13:36:28 +0700

    Improve lab2

--- lab2/main.c
+++ lab2/main.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 #include <immintrin.h>
 
 #ifndef _TYPE_SIZE
@@ -23,8 +25,36 @@ typedef __m256 current_type_t;
 #error Unknown _TYPE_SIZE macros value
 #endif
 
+#define BYTES_COUNT (1024 * 1024 * 100)
+
 int main(int argc, char* argv[]) {
-  printf("type size: %lu\n", sizeof(current_type_t));
+  size_t size = BYTES_COUNT - (BYTES_COUNT % sizeof(current_type_t));
+  size_t elems_count = BYTES_COUNT / sizeof(current_type_t);
+  printf("Allocating %lu elements on %lu bytes...\n", elems_count, size);
+  current_type_t* src = (current_type_t*) malloc(size);
+  current_type_t* dst = (current_type_t*) malloc(size);
+
+  printf("Start copying...\n");
+  // TODO - begin timer
+
+  for (size_t i = 0; i < elems_count; i++)
+    dst[i] = src[i];
+
+  // TODO - end timer
+  printf("Copy finished in:\n");
+
+  printf("Start memcpy test...\n");
+  // TODO - begin timer
+
+  memcpy(dst, src, size);
+
+  // TODO - end timer
+  printf("Memcpy finished in:\n");
+
+  fprintf(stderr, "Array depend: %lu, %lu\n", (size_t) (src + size), (size_t) (dst + size));
+
+  free(src);
+  free(dst);
   return 0;
 }
 
--- lab2/run.sh
+++ lab2/run.sh
@@ -3,7 +3,7 @@
 for size in 1 2 4 8 16 32
 do
   echo "$size byte(s):"
-  time ./mem-test-$size
+  ./mem-test-$size
   echo "================"
 done
 
