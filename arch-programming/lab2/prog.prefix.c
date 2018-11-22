31b73b6641dda00d7aa4c086f8690f9c26098b07
Kirill Polushin <kirill@polushin.org>
2018-11-22 21:26:11 +0700

    Finish lab 2 in arch

--- arch-programming/lab2/prog.prefix.c
+++ arch-programming/lab2/prog.prefix.c
@@ -1,24 +1,62 @@
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <x86intrin.h>
 #include <time.h>
-#define ARRAY_SIZE (200 * 1024 * 1024 / sizeof(int))
+
+#define ARRAY_SIZE (12 * 1024 * 1024 / sizeof(int))
 
 int arr[ARRAY_SIZE];
-int arr2[ARRAY_SIZE];
+
+void arr_random_init() {
+    /*int x = 0, ind;
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+        arr[i] = ARRAY_SIZE;
+
+    for (int k = 0; k < ARRAY_SIZE - 1; ++k) {
+        while (arr[ind = rand() % ARRAY_SIZE] != ARRAY_SIZE || ind == x);
+        arr[x] = ind;
+        x = ind;
+    }
+
+    arr[x] = 0;*/
+
+    size_t pos = 0, prev = 0;
+    memset(arr, -1, sizeof(arr));
+
+    for (size_t i = 0; i < ARRAY_SIZE; i++) {
+        pos = rand() % ARRAY_SIZE;
+        while (arr[pos] != -1) {
+            if (pos++ >= ARRAY_SIZE)
+                pos = 0;
+        }
+
+        arr[pos] = prev;
+        prev = pos;
+    }
+
+    arr[0] = prev;
+}
 
 int main(int argc, const char *argv[]) {
-    srand(time(NULL));
-    for (int i = 0; i < ARRAY_SIZE; ++i) {
-        arr[i] = rand() % ARRAY_SIZE;
-    }
-    unsigned int iter_num  = atoi(argv[1]);
+    unsigned int iter_num = atoi(argv[1]);
     uint64_t start;
     uint64_t end;
-    double aver = 0;
-    uint64_t min = 0;
-    for (int j = 0; j < 10; j++) {
+    int k = 0;
+
+    srand(time(NULL));
+    fprintf(stderr, "Init array...\n");
+    arr_random_init();
+    fprintf(stderr, "Computing...\n");
+
+    int* trash = (int *) calloc(sizeof(int), ARRAY_SIZE);
+    for (size_t i = 0; i < ARRAY_SIZE; ++i)
+        trash[i] = i;
+
+    for (int i = 0; i < 400; i++)
+        __asm__("nop");
+
     start = __rdtsc();
-    for (int i = 0; i < iter_num; ++i) {
-        arr2[arr[i]] = arr[i];
+    for (size_t i = 0; i < iter_num; i++) {
+        k = arr[k];
--- arch-programming/lab2/run.sh
+++ arch-programming/lab2/run.sh
@@ -1,14 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-rm result.csv prog.c test
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+  echo "Args: <nops-min> <nops-max> <repeats>"
+  exit 1
+fi
 
-for i in $(seq 10 $1)
+rm result.csv
+
+for i in $(seq $1 $2)
 do
   cat prog.prefix.c > prog.c
 
   for j in $(seq 1 $i)
   do
-    echo "__asm__(\"nop\");" >> prog.c
+    echo "        __asm__(\"nop\");" >> prog.c
   done
 
   cat prog.suffix.c >> prog.c
@@ -17,7 +22,7 @@ do
   gcc -O2 prog.c -o prog
   echo "===== Running $i =========="
 
-  RESULT=$(./prog $2)
+  RESULT=$(./prog $3)
   echo $RESULT
   echo "$i,$RESULT" >> result.csv
 
